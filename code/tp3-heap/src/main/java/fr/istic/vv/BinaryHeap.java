d8e61fac3c3e9d9470add5a7c64232a2ad67cc23
doctorpest <103544303+doctorpest@users.noreply.github.com>
2024-10-09 12:08:39 +0200

    Update BinaryHeap.java

--- code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -42,15 +42,15 @@ class BinaryHeap<T> {
         list.add(element);
         restoreOrderUp(list.size() - 1);
     }
-// count the number of elements in the heap
+// count the number of elements in the heap, the size of the heap
     public int count() { return list.size();}
-    //reorder the heap after insertion of a new element from bottom to top
+    //reorder the heap after insertion of a new element from bottom to top, when there's a new push to put the addes element on the right place
     private void restoreOrderUp(int i) {
         T temp = list.get(i);
         while (i >0){
-            int j = (i - 1) / 2; //indice du parent
+            int j = (i - 1) / 2; //index of the parent
             T parent = list.get(j);
-            if (comparator.compare(temp, parent) < 0) {
+            if (comparator.compare(temp, parent) < 0) { // check if the child is greater than the parent so we change the order if it is
                 list.set(i, parent);
                 i = j;
             } else {
@@ -59,7 +59,7 @@ class BinaryHeap<T> {
         }
         list.set(i, temp);
     }
-    //reorder the heap after removing the root from top to bottom
+    //reorder the heap after removing the root from top to bottom , tp put the new root who is normally the second smallest after the root
     private void restoreOrderDown(int i) {
         T temp = list.get(i);
         int s = list.size();
@@ -67,10 +67,10 @@ class BinaryHeap<T> {
             int j = i * 2 + 1; //leftchild
             int k = j + 1;//rightchild
             int smallest = j;//assume is the right one
-            if (k < s && comparator.compare(list.get(k), list.get(j)) < 0) {
+            if (k < s && comparator.compare(list.get(k), list.get(j)) < 0) { // comparison between the right and left child seeing who dominates more, the smallest between both of them
                 smallest = k;
             }
-            if (comparator.compare(temp,list.get(smallest)) <= 0) {
+            if (comparator.compare(temp,list.get(smallest)) <= 0) { //if it equals don't change anything 
                 break;
             }
             list.set(i,list.get(smallest));
